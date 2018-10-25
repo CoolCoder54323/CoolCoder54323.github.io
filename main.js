cbb05d5e6962294ec5695d676d1674039d90976c
CoolCoder54323 <nicxellent@icloud.com>
2018-10-25 06:53:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -318,10 +318,10 @@ function no() {
            0)
       break;
     case 2:
-      jump("",
-           "",
-           "",
-           0)
+      jump("You jump out your window thinking that you have avoided the Wampanoag Indians and all harm that they could cause you only to see 8 different Wampanoag Indians surrounding as they begin to stab you to death.",
+           "reset",
+           "reset",
+           1)
       break;
     case 3:
       jump("",
@@ -336,10 +336,10 @@ function no() {
            0)
             break;
     case 5:
-      jump("",
-           "",
-           "",
-           0)
+      jump("You lunge at the Indian with the bow straining your arm towards his leg and gashing it deeply, he falls over as you make your swift escape past the tomahawk. You take a quick look around with no Indians within your vision, suddenly you realize that through all of the adrenaline the Wampanoag with the tomahawk had sliced your leg and blood was oozing down. Do you...",
+           "Search a raided house for medical supplies",
+           "Leave into the forest through the cold climate ",
+           7)
             break;
     case 6:
       jump("",
@@ -354,22 +354,22 @@ function no() {
            0)
             break;
     case 8:
-      jump("",
-           "",
-           "",
-           0)
+      jump("You run into the forest towards the nearest settlement in search of help, but while you run your wound gets filthy and infected. Do you...",
+           "Head back to the invaded settlement to search for medical supplies ",
+           "Tough it out and stop acting like a wuss",
+           9)
             break;
     case 9:
-      jump("",
-           "",
-           "",
-           0)
+      jump("You decide that it’s not worth going back to the settlement because of the Wampanoag and continue heading towards to the nearest settlement to consult the reset of your fellow English. You decide to rest for a little as you are tired from fighting. Do you...",
+           "Sleep for an hour",
+           "Stay awake but rest",
+           10)
             break;
     case 10:
-      jump("",
-           "",
-           "",
-           0)
+      jump("You fall asleep for about 40 minutes not getting a full hour because you wake up in excruciating pain. You stare down at your wound as your vision goes blurry and you pass out and perish.",
+           "reset",
+           "reset",
+           1)
             break;
     case 11:
       jump("",
