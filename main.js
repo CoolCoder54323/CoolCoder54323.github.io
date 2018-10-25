14881720d69c64c56dc14ee0b3681b28aca59a2d
CoolCoder54323 <nicxellent@icloud.com>
2018-10-25 07:04:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -348,23 +348,21 @@ function no() {
            0)
             break;
     case 7:
-      jump("",
-           "",
-           "",
-           0)
-            break;
-    case 8:
       jump("You run into the forest towards the nearest settlement in search of help, but while you run your wound gets filthy and infected. Do you...",
            "Head back to the invaded settlement to search for medical supplies ",
            "Tough it out and stop acting like a wuss",
            9)
             break;
-    case 9:
+    case 8:
       jump("You decide that it’s not worth going back to the settlement because of the Wampanoag and continue heading towards to the nearest settlement to consult the reset of your fellow English. You decide to rest for a little as you are tired from fighting. Do you...",
            "Sleep for an hour",
            "Stay awake but rest",
            10)
             break;
+    case 9:
+      jump("You fall asleep for about 40 minutes not getting a full hour because you wake up in excruciating pain. You stare down at your wound as your vision goes blurry and you pass out and perish.",
+           "reset",
+           "reset",
     case 10:
       jump("You fall asleep for about 40 minutes not getting a full hour because you wake up in excruciating pain. You stare down at your wound as your vision goes blurry and you pass out and perish.",
            "reset",
