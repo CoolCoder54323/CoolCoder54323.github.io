8555ea6ce6dcf16065bba82f448b4138d389adb3
CoolCoder54323 <nicxellent@icloud.com>
2018-10-24 23:25:43 -0400

    reset

--- main.js
+++ main.js
@@ -10,34 +10,11 @@ function yes() {
   var story = document.getElementById("story").innerHTML
   switch (state) {
 
+    case 0:
     case 1:
-      document.getElementById("story").innerHTML = "You wake up with screaming piercing your ears from outside from the cold Southern English environment. You sit up startled and dazed as two Indians break through your door. It’s the Wampanoag Indians, you have a peace treaty with them, but it has been slowly crumbling over the years. Do you...";
-      document.getElementById("yes").innerHTML = "fight";
-      document.getElementById("no").innerHTML = "jump out window";
-
-
-
-      state = 2;
-      break;
     case 2:
-      document.getElementById("story").innerHTML = story + "<br> <br> You pick up your knife off of the bedside table and stare the Indians down scanning them to decide your best chance of surviving. One of the Wampanoag is holding a tomahawk while the other is holding a bow. Do you attack the...";
-      document.getElementById("yes").innerHTML = "tomohawk";
-      document.getElementById("no").innerHTML = "bowman";
-      state = 3;
-      break;
-
-    case 3:      
-      document.getElementById("story").innerHTML = story + "<br> <br> You lunge at the man holding the tomahawk and stab him in the chest. He screams in agony dropping his weapon. The bow attempts to shoot you, missing by an inch hitting the wall. You make your quick escape and stare behind to make sure no one is chasing you. Looking back may not have been the best decision, considering that you instantly fall straight onto your knife and it impales your leg. Do you...";
-      document.getElementById("yes").innerHTML = "ignore the bleeding wound completely.";
-      document.getElementById("no").innerHTML = "Search an abandoned house for medical supplies";
-      state = 15;
-      break;
+    case 3:
     case 4:
-      document.getElementById("story").innerHTML = "what do you want to be";
-      document.getElementById("yes").innerHTML = "Englishmen";
-      document.getElementById("no").innerHTML = "Native";
-      state = 1
-      break;
     case 5:
     case 6:
     case 7:
@@ -49,11 +26,6 @@ function yes() {
     case 13:
     case 14:
     case 15:
-      document.getElementById("story").innerHTML = story + "<br> <br>You bleed to death. Now that was just stupid. What did you expect?";
-      document.getElementById("yes").innerHTML = "reset";
-      document.getElementById("no").innerHTML = "reset";
-      state = 1
-      break;
     case 16:
     case 17:
     case 18:
@@ -89,50 +61,14 @@ function no() {
   var story = document.getElementById("story").innerHTML
   switch (state) {
 
-
+    case:0
     case 1:
-
-      break;
     case 2:
-      document.getElementById("story").innerHTML = story + "<br> <br>You jump out your window thinking that you have avoided the Wampanoag Indians and all harm that they could cause you only to see 8 different Wampanoag Indians surrounding as they begin to stab you to death.";
-      document.getElementById("yes").innerHTML = "reset";
-      document.getElementById("no").innerHTML = "reset";
-
-      state = 4;
-      break;
     case 3:
-      document.getElementById("story").innerHTML = story + "<br> <br>You run to a house that appears to have already been raided searching for bandages, and much to your luck you discover bandages and a cloth to keep your wound clean. Do you...";
-      document.getElementById("yes").innerHTML = "Search a raided house for medical supplies";
-      document.getElementById("no").innerHTML = "Leave into the forest through the cold climate ";
-
-
-
-      state = 7;
-      break;
     case 4:
-      document.getElementById("story").innerHTML = "what do you want to be";
-      document.getElementById("yes").innerHTML = "Englishmen";
-      document.getElementById("no").innerHTML = "Native";
-      state = 1
-      break;
     case 5:
-      document.getElementById("story").innerHTML = story + "<br> <br>You run to a house that appears to have already been raided searching for bandages, and much to your luck you discover bandages and a cloth to keep your wound clean. Do you...";
-      document.getElementById("yes").innerHTML = "find a safe hiding spot outside then apply bandages";
-      document.getElementById("no").innerHTML = "Apply bandages in the house";
-      state = 7
-      break;
     case 6:
-      document.getElementById("story").innerHTML = story + "<br> <br>You clean off your wound and put the bandages on. Whilst you are almost finished applying that bandage you hear many people walking below you. Do you..";
-      document.getElementById("yes").innerHTML = "Inspect the footsteps";
-      document.getElementById("no").innerHTML = "Ditch out the window ";
-      state = 7
-      break;
     case 7:
-      document.getElementById("story").innerHTML = story + "<br> <br>You clean off your wound and put the bandages on. Whilst you are almost finished applying that bandage you hear many people walking below you. Do you..";
-      document.getElementById("yes").innerHTML = "Inspect the footsteps";
-      document.getElementById("no").innerHTML = "Ditch out the window ";
-      state = 7
-      break;
     case 8:
     case 9:
     case 10:
@@ -141,11 +77,6 @@ function no() {
     case 13:
     case 14:
     case 15:
-      document.getElementById("story").innerHTML = "You bleed to death. Now that was just stupid. What did you expect?";
-      document.getElementById("yes").innerHTML = "reset";
-      document.getElementById("no").innerHTML = "reset";
-      state = 1
-      break;
     case 16:
     case 17:
     case 18:
@@ -176,8 +107,10 @@ function no() {
   scrollD()
 }
 
+
 function scrollD() {
   var objDiv = document.getElementById("story");
   objDiv.scrollTop = objDiv.scrollHeight;
 }
+func jump(string)
 
